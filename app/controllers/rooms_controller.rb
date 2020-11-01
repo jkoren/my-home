186c0a6bee3af9fed97f7715414641fd676e0bc2
jkoren <j.korenstein@gmail.com>
2020-10-31 22:42:14 -0400

    starting controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::RoomsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    rooms = Room.all
+    render json: rooms
+  end
+
+  def index
+    room = Room.find(params[:id])
+    # render json: {
+    #   room: serialized_data(movie, RoomSerializer),
+    #   possessions: serialized_data(possession.reviews, PosessionSerializer)
+    # }
+  end
+
+end
