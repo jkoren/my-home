37a2377f34eaa836039b66d670c63ededed58645
jkoren <j.korenstein@gmail.com>
2020-11-02 09:41:03 -0500

    issue w .new and .create

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,23 @@
 User.destroy_all
-dave = User.new(email: "dave@gmail.com")
+dave = User.create(email: "dave@gmail.com")
 dave.password = "testtest"
-dave.save
-bob = User.new(email: "bob@gmail.com")
+
+bob = User.create(email: "bob@gmail.com")
 bob.password = "testtest"
-bob.save
 
 living_room_image = File.open(File.join( Rails.root, 'app/assets/images/seed_images/living_room.jpg'))
 kitchen_image = File.open(File.join( Rails.root, 'app/assets/images/seed_images/kitchen.jpg'))
 master_bedroom_image = File.open(File.join( Rails.root, 'app/assets/images/seed_images/master_bedroom.jpg'))
 
 Room.destroy_all
-living_room = Room.new(name: "Living Room",description: "Where we hang out mostly")
+living_room = Room.create(name: "Living Room",description: "Where we hang out mostly")
 living_room.image = living_room_image
 living_room.user = dave
 
-kitchen = Room.new(name: "Kitchen",description: "State of the Art")
+kitchen = Room.create(name: "Kitchen",description: "State of the Art", user: dave, image: kitchen_image)
 kitchen.image = kitchen_image
 kitchen.user = dave
 
-master_bedroom = Room.new("Master Bedroom","Spacious")
+master_bedroom = Room.create("Master Bedroom","Spacious")
 master_bedroom.image = master_bedroom_image
 master_bedroom.user = dave
