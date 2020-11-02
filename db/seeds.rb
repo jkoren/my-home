fda28a6f746f5017ca9ab74f70b3a39a80548b5c
jkoren <j.korenstein@gmail.com>
2020-11-02 14:09:26 -0500

    seed-data-and-rspec-user-test-and-fixed-model-filenames

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,16 @@
 User.destroy_all
-dave = User.create(email: "dave@gmail.com")
-dave.password = "testtest"
+dave = User.create(email: "dave@gmail.com",password: "testtest")
 
-bob = User.create(email: "bob@gmail.com")
-bob.password = "testtest"
+bob = User.create(email: "bob@gmail.com", password: "testtest")
 
 living_room_image = File.open(File.join( Rails.root, 'app/assets/images/seed_images/living_room.jpg'))
 kitchen_image = File.open(File.join( Rails.root, 'app/assets/images/seed_images/kitchen.jpg'))
 master_bedroom_image = File.open(File.join( Rails.root, 'app/assets/images/seed_images/master_bedroom.jpg'))
 
-# Room.destroy_all
-# living_room = Room.create(name: "Living Room",description: "Where we hang out mostly")
-# living_room.image = living_room_image
-# living_room.user = dave
+Room.destroy_all
 
-# kitchen = Room.create(name: "Kitchen",description: "State of the Art", user: dave, image: kitchen_image)
-# kitchen.image = kitchen_image
-# kitchen.user = dave
+living_room = Room.create(name: "Living Room",description: "Where we hang out mostly", image: living_room_image, user: dave)
 
-# master_bedroom = Room.create("Master Bedroom","Spacious")
-# master_bedroom.image = master_bedroom_image
-# master_bedroom.user = dave
+kitchen = Room.create(name: "Kitchen",description: "State of the Art", image: kitchen_image, user: dave)
+
+master_bedroom = Room.create(name:"Master Bedroom",description: "Spacious", image: master_bedroom_image, user: dave)
