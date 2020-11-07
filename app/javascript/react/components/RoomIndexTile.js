37697c9f63d59d9e702219a9bfc4989d5768d76f
jkoren <j.korenstein@gmail.com>
2020-11-06 23:39:22 -0500

    added seed images and cleaned up tiles

--- app/javascript/react/components/RoomIndexTile.js
+++ app/javascript/react/components/RoomIndexTile.js
@@ -3,22 +3,14 @@ import { Link } from "react-router-dom";
 
 const RoomIndexTile = (props) => {
   return(
-    <div className="callout cell small-12 medium-3 hover-zoom box-shadow">
+    <div className="callout cell small-12 medium-4 hover-zoom box-shadow">
       <Link to={`/rooms/${props.data.id}`}>
         <h4 className="field">{props.data.name}</h4>
         <h5 className="field">{props.data.description} </h5>
       
         <div className="text-center">
-          <img src={props.data.image} alt="room image" width="150" />
+          <img src={props.data.image} alt="room image" width="250" />
         </div>
-
-        <div className="text-center">
-          <img src="https://secure.img1-fg.wfcdn.com/im/49278377/resize-h600%5Ecompr-r85/5706/57068565/Portland+4+-+Light+Kitchen+Island+Linear+Pendant.jpg" alt="room image" width="150" />
-        </div>
-
-        {/* <div className="text-center">
-          <img src={duck.duck_photo} alt="room image" width="150" />
-        </div> */}
 
       </Link>
     </div>
