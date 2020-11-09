3199d396b78fa72a5f1303f99a46f3803875b285
jkoren <j.korenstein@gmail.com>
2020-11-08 23:08:53 -0500

    cleaned up possession show tile

--- app/serializers/possession_show_serializer.rb
+++ app/serializers/possession_show_serializer.rb
@@ -1,5 +1,5 @@
 class PossessionShowSerializer < ActiveModel::Serializer 
-  attributes :id, :name, :manufacturer, :model, :owners_manual, :description, :image, :URL, :operating_video
+  attributes :id, :name, :manufacturer, :model, :owners_manual, :description, :year_built, :purchased_from, :image, :purchase_date, :purchase_receipt, :purchase_price, :URL, :operating_video, :URL
 
   belongs_to :room
 end
