dc94a68fb0efb02630277f36d59db6b21aa54d62
jkoren <j.korenstein@gmail.com>
2020-11-12 16:10:36 -0500

    more work on api yelp call

--- app/serializers/possession_show_serializer.rb
+++ app/serializers/possession_show_serializer.rb
@@ -1,5 +1,13 @@
 class PossessionShowSerializer < ActiveModel::Serializer 
   attributes :id, :name, :manufacturer, :model, :owners_manual, :description, :year_built, :purchased_from, :image, :purchase_date, :purchase_receipt, :purchase_price, :URL, :operating_video, :URL, :warranty
+  # , :professional
 
   belongs_to :room
+
+  def professional
+    client = Professional.new("02451")
+    professional = client.format_yelp_api_response
+    return professional
+  end
+
 end
