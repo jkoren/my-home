5fb5d846f0f40651514991493c03bddd541c0433
jkoren <j.korenstein@gmail.com>
2020-11-26 17:02:21 -0500

    made user association to residence optional for automated user testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :residence
+  belongs_to :residence, optional: true
+  # may want to remove optional: true later
 end
