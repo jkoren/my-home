824a7d97b32be7a44e1cb8ef85c62b016ef0cb85
jkoren <j.korenstein@gmail.com>
2020-12-16 00:06:18 -0500

    fixed

--- app/javascript/react/components/possessions/PossessionsIndexContainer.js
+++ app/javascript/react/components/possessions/PossessionsIndexContainer.js
@@ -15,6 +15,7 @@ const PossessionsIndexContainer = (props) => {
       )
     })
   }
+  // debugger
   return (
     <div>
       <div className="grid-x grid-margin-x" style={{ height: "100%", padding: "0 2rem" }}>
