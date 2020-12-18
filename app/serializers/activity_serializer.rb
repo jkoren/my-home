1f137ab92c3b0d81ecae7db38ba861c148f59d92
jkoren <j.korenstein@gmail.com>
2020-12-18 11:36:00 -0500

    fixed for case when user was deleted

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -1,7 +1,12 @@
+# activity_serializer.rb
 class ActivitySerializer < ActiveModel::Serializer 
   attributes :action, :table, :name, :id_of_item, :email, :created_at
 
   def email
-    object.user.email
+    if object.user
+      object.user.email
+    else  
+      "user not found"
+    end
   end
 end
