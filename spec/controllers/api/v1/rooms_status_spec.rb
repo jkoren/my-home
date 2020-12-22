52ea2a97ce3c73d05268058109b31b826a9f4df3
jkoren <j.korenstein@gmail.com>
2020-12-22 05:38:53 -0500

    removed incomplete rooms rspec test

--- spec/controllers/api/v1/rooms_status_spec.rb
+++ spec/controllers/api/v1/rooms_status_spec.rb
@@ -3,11 +3,6 @@ require "rails_helper"
 
 RSpec.describe Api::V1::RoomsController, type: :controller do 
   describe 'GET index' do
-    it 'returns a status message' do
-      get ('index')
-      binding.pry
-      json = JSON.parse(response.body)
-      expect(json['status']).to eql('ok')
-    end
   end
+  
 end
