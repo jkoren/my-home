9d5742a98ee3ef9653abd04eb9a76bcd336f99f4
jkoren <j.korenstein@gmail.com>
2020-12-24 16:57:50 -0500

    changed privacy wording to allow

--- db/migrate/20201224205351_add_leaderboard_privacy_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddLeaderboardPrivacyToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :leaderboard_privacy, :boolean,  default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_205351) do
+ActiveRecord::Schema.define(version: 2020_12_24_215055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 2020_12_24_205351) do
     t.bigint "residence_id"
     t.string "role", default: "member", null: false
     t.string "screen_name", default: "-", null: false
-    t.boolean "leaderboard_privacy", default: false
+    t.boolean "allow_leaderboard", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["residence_id"], name: "index_users_on_residence_id"
